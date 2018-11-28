ee7fe579850e216aa502db41a078458f37c4f0b7
Sergii Zaitsev <sergii.zaitsev@sixt.com>
2018-11-28 15:21:17 +0100

    update drumm

--- drumm/sw.js
+++ drumm/sw.js
@@ -1,15 +1,26 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      return cache.add('index.html');
-    })
-  );
-});
- 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
-    })
-  );
+const cacheName = 'files';
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(async function() {
+    const responseFromFetch = fetch(request);
+    event.waitUntil(async function() {
+      const responseCopy = (await responseFromFetch).clone();
+      const myCache = await caches.open(cacheName);
+      await myCache.put(request, responseCopy);
+    }());
+    if (request.headers.get('Accept').includes('text/html')) {
+      try {
+        return await responseFromFetch;
+      } catch(error) {
+        return caches.match(request);
+      }
+    } else {
+      const responseFromCache = await caches.match(request);
+      return responseFromCache || responseFromFetch;
+    }
+  }());
 });
